58cac1b5c7482be528ae1abd8a87a534e1dc6017
James Doss-Gollin <j.dossgollin@gmail.com>
2015-11-19 17:22:10 -0500

    some further updates to skills

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ pdflatex main
 biber main --output-safechars
 pdflatex main
 pdflatex main
-rm main.bbl main.log main.run.xml main.bcf main.out main.aux main.blg
+rm main.bbl main.log main.run.xml main.bcf main.out main.aux main.blg *.bak
+cp main.pdf ../CVDossGollin.pdf

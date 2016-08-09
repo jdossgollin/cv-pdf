1823ba058dc17130efd0dd4182c1b84656677719
James Doss-Gollin <james.dossgollin@gmail.com>
2016-08-09 16:40:00 -0400

    rename CV name

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ pdflatex main
 biber main --output-safechars
 pdflatex main
 pdflatex main
-rm main.bbl main.log main.run.xml main.bcf main.out main.aux main.blg *.bak
-mv main.pdf CV-james-dossgollin.pdf
+rm main.bbl main.log main.run.xml main.bcf main.out main.aux main.blg
+mv main.pdf CV_Doss-Gollin_James.pdf

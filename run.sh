a5306d8db67e5e49fe0508ff53c750eeb29ec116
James Doss-Gollin <james.dossgollin@gmail.com>
2018-02-21 10:02:21 -0500

    re-organize

--- run.sh
+++
@@ -1,6 +0,0 @@
-pdflatex main
-biber main --output-safechars
-pdflatex main
-pdflatex main
-rm main.bbl main.log main.run.xml main.bcf main.out main.aux main.blg
-mv main.pdf CV_Doss-Gollin_James.pdf

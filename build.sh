6529e92bd87e6c8c921582f7b26a31c38c4c1049
James Doss-Gollin <james.doss-gollin@columbia.edu>
2020-12-29 08:20:30 -0500

    some word edits

--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@ xelatex CV_Doss-Gollin_James
 biber CV_Doss-Gollin_James
 xelatex CV_Doss-Gollin_James
 cp CV_Doss-Gollin_James.pdf docs/
-open docs/CV_Doss-Gollin_James.pdf
+
+xelatex biosketch_Doss-Gollin_James
+xelatex biosketch_Doss-Gollin_James
+biber biosketch_Doss-Gollin_James
+xelatex biosketch_Doss-Gollin_James
+cp biosketch_Doss-Gollin_James.pdf docs/
